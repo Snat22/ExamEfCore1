ae12e2edc241c7cde0de5121cc130a46a0127a83
Sins <142435643+Snat22@users.noreply.github.com>
2024-04-15 14:01:30 +0500

    Update Challenge.cs

--- Domain/Models/Challenge.cs
+++ Domain/Models/Challenge.cs
@@ -6,5 +6,5 @@ public class Challenge
     public string? Title { get; set; }
     public string? Description { get; set; }
 
-    public ICollection<Group>? Groups { get; set; }
+    public List<Group>? Groups { get; set; }
 }
