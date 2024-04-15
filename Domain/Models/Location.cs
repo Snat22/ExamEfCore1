6c59bcc98a72aeeeb5774dc9b205d77ef011c679
Sins <142435643+Snat22@users.noreply.github.com>
2024-04-15 14:00:59 +0500

    Update Location.cs

--- Domain/Models/Location.cs
+++ Domain/Models/Location.cs
@@ -6,5 +6,5 @@ public class Location
     public string? Name { get; set; }
     public string? Description { get; set; }
 
-    public ICollection<Participant>? Participants { get; set; }
+    public List<Participant>? Participants { get; set; }
 }
